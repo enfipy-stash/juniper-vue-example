f4c09d1af65493865210cefc57b01152672292d3
enfipy <enfipy@gmail.com>
2020-02-27 18:53:37 +0200

    Add comments and protocols

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,7 +3,7 @@ mod database;
 mod models;
 mod schema;
 
-use actix::{Actor, StreamHandler};
+use actix::{Actor, StreamHandler, ActorContext};
 use actix_web::{
     get, http::{header, StatusCode}, post, web, App, Error, FromRequest, HttpRequest, HttpResponse, HttpServer, Responder,
 };
@@ -36,14 +36,53 @@ async fn graphql_handler(
     Ok(HttpResponse::Ok().content_type("application/json").body(res))
 }
 
-struct JuniperWebSocket;
+// #[juniper::graphql_subscription(Context = JuniperContext)]
+// impl Subscription {
+//     async fn subscribe_for_messages(ctx: &JuniperContext) -> Stream<Message> {
+//         match ctx.ws_message {
+//             Message::Connected(token) => {
+//                 let user = parse_jwt_toke(token);
+//                 chat.join_user(user);
+//                 chat_usecase.subscribe_for_messages(user)
+//             }
+//             Message::Disconnected() => {
+//                 chat.left_user(user);
+//                 stream::empty::<Message>()
+//             }
+//         }
+//     }
+// }
+
+
+struct JuniperWebSocketActor;
+
+impl Actor for JuniperWebSocketActor {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        log::info!("started");
+        // let context = JuniperContext::init(database.get_ref().clone());
+        // feature =
+            // let stream = req.execute_subscribe(&graphql_root, &context, &ctx, token, [arguments])
+            // while let Some(msg) = stream.next() {
+                // ctx.text(msg)
+                // }
+        // self.spawn(feature)
+    }
+}
+
+struct JuniperWebSocket; 
+// {
+//     user_id: Uuid,
+//     request_ids_map: Map<i32, JuniperWebSocketActor>,
+// }
 
 impl Actor for JuniperWebSocket {
     type Context = ws::WebsocketContext<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
         log::info!("started");
-        ctx.text(r#"{{"lol":"O_O"}}"#)
+        ctx.text(r#"{"lol":"O_O"}"#)
     }
 }
 
@@ -52,11 +91,26 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for JuniperWebSocket 
         log::info!("msg: {:?}", msg);
         match msg {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Text(text)) => {
+                ctx.text(text)
+                // let request_id = text.request_id;
+                // create actor and add to map
+                // with key: user_id, request_id
+                // with value: JuniperWebSocketActor
+                // start_abiter(start)
+            },
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Close(_)) => {
+                // Iterate by self.request_ids_map
+                // Remove all self.actors
+                ctx.stop()
+            },
             _ => (),
         }
     }
+
+    // fn closed
+        // Send message to every actor with message: Disconnected
 }
 
 #[get("/subscriptions")]
@@ -72,11 +126,11 @@ async fn subscriptions_handler(
     //     bytes.extend_from_slice(&item?);
     // }
     // log::info!("Body {:?}!", bytes);
-    //log::info!("req: {:?}", req);
-    ws::start(JuniperWebSocket, &req, stream)
+    // let mut nstream = stream.into_inner();
+    // let greq = web::Json::<GraphQLRequest>::from_request(&req, &mut nstream).await;
+    log::info!("req: {:?}", req);
+    ws::start_with_protocols(JuniperWebSocket, &["graphql-ws"], &req, stream)
     //log::info!("Sub: {:?}", res);
-    // let mut stream = stream.into_inner();
-    // let greq = web::Json::<GraphQLRequest>::from_request(&req, &mut stream).await;
     //log::info!("greq: {:?}", greq);
     //log::info!("graphql_root: {:?}", graphql_root);
     //res
